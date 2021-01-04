14f29c9455731a1c7bee48de32708871f7560585
bkupz <grandbman1@gmail.com>
2021-01-04 07:54:55 -0700

    added loggging to clock

--- clock.py
+++ clock.py
@@ -1,10 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
 import data_utils.data as data
 
 sched = BlockingScheduler()
 
 
 def timed_job():
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    print("Adding to db")
+    print("Current Time =", current_time)
     data.addHourDataToDB()
 
 sched.add_job(timed_job, 'cron', minute=5)
