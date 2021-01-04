ef0e1029c624eda8d01a31cf5c171e874045ee46
bkupz <grandbman1@gmail.com>
2021-01-03 19:29:18 -0700

    Heroku configs

--- alta_weather_history/settings.py
+++ alta_weather_history/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os as os
 import dj_database_url
 import dotenv
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
