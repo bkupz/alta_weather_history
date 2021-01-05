95e792288622d1c6ff37a89fb209db7d1ce15101
bkupz <grandbman1@gmail.com>
2021-01-04 22:26:45 -0700

    Changed a timezone

--- clock.py
+++ clock.py
@@ -1,12 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
 import data_utils.data as data
+from pytz import timezone
 
 sched = BlockingScheduler()
 
-
 def timed_job():
-    now = datetime.now()
+    now = datetime.now(timezone('America/Denver'))
 
     current_time = now.strftime("%H" + ":00")
     day = now.strftime('%-m/%-d/%Y')
@@ -17,8 +17,6 @@ def timed_job():
         print("Current Time =", current_time) 
         data.addHourDataToDB()
         print("done updating db")
-        current_time = now.strftime("%H" + ":00")
-        print("Current Time =", current_time)
 
 sched.add_job(timed_job, 'interval', minutes=15)
 sched.start()
